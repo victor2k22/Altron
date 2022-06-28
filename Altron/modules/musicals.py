560979cc12c0f05e5ffe4fb3d7d9f3c0361a6fae
TheAltronX <108298020+TheAltronX@users.noreply.github.com>
2022-06-29 02:45:01 +0530

    Add files via upload

---
+++ Altron/modules/musicals.py
@@ -0,0 +1,6 @@
+__mod_name__ = "Mᴜsɪᴄ"
+
+__help__ = """
+ /song <song name>*:* Uploads the song in it's best quality available
+ 💡Ex: `/song Faded Alan Walker`
+"""
---
+++ Altron/modules/tools.py
@@ -0,0 +1,10 @@
+# © @TheAltronX
+
+__mod_name__ = "Tᴏᴏʟs"
+
+__help__ = """
+
+*Converts*
+ /encrypt*:* Encrypts The Given Text
+ /decrypt*:* Decrypts Previously Ecrypted Text
+"""
