7394c309eafdde14ecf14d1874f75d5dc1e0eb18
TheAltronX <108298020+TheAltronX@users.noreply.github.com>
2022-06-30 04:59:27 +0530

    Add files via upload

--- Altron/modules/modules.py
+++ Altron/modules/modules.py
@@ -24,12 +24,7 @@ def listmodules(update: Update, context: CallbackContext):
     message.reply_text(module_list, parse_mode=ParseMode.HTML)
 
 
-LOAD_HANDLER = CommandHandler("load", load)
-UNLOAD_HANDLER = CommandHandler("unload", unload)
 LISTMODULES_HANDLER = CommandHandler("listmodules", listmodules)
-
-dispatcher.add_handler(LOAD_HANDLER)
-dispatcher.add_handler(UNLOAD_HANDLER)
 dispatcher.add_handler(LISTMODULES_HANDLER)
 
 __mod_name__ = "Modules"
